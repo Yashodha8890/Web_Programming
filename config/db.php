f7efb4aefba8d7b69eb19800e705c808b7589982
Kapila Bandara <kapila23000@student.hamk.fi>
2024-02-15 15:09:38 +0200

    Cutomer feedback and cutomer rating changes added.

--- config/db.php
+++ config/db.php
@@ -1,10 +1,11 @@
 <?php
-    $serverName = "web-dev-env-main-db-1";
+    $serverName = "php24-db-1";
     $userName = "root";
     $password = "password";
     // $dbName = "web_programing_project_db";
     //$dbName = "udashi";
-     $dbName = "project";
+    // $dbName = "project";
+    $dbName = "web_programm_project_db";
 
     //create the connection with the Database
 
